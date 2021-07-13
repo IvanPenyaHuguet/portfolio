a46c1ed910e88796cfce1a65d8d7df09b21b9374
Iván Peña Huguet <ivanpenyahuguet@gmail.com>
2021-07-14 00:11:16 +0200

    Global styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,18 @@ import GlobalStylesCustom from "../styles/GlobalStylesCustom";
 
 import { useTranslation } from "next-i18next";
 
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+}
+
 export default function Index() {
-  const { t } = useTranslation("footer");
+  const { t } = useTranslation("common");
   return (
     <div className="App">
       <GlobalStylesCustom />
