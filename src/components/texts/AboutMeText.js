359c75a138f64d20db90ab3b3314e7ec86813075
ivanpenyahuguet@gmail.com <ivanpenyahuguet@gmail.com>
2021-07-27 23:48:13 +0200

    About me text

--- src/components/texts/AboutMeText.js
+++ src/components/texts/AboutMeText.js
@@ -6,14 +6,18 @@ const TextContainer = styled.article({
 });
 
 const Title = styled.h2({
-  ...tw`tracking-wider text-4xl font-bold mb-5`,
+  ...tw`tracking-wider font-title
+  text-5xl font-bold mb-5 
+  sm:text-4xl xs:text-3xl`,
 });
 const Text = styled.div({
-  ...tw``,
+  ...tw`font-text text-justify
+     text-xl font-medium sm:text-lg xs:text-base`,
 });
 
 const Paragraph = styled.p({
-  ...tw``,
+  ...tw`mb-8 sm:mb-6 xs:mb-4`,
+  textIndent: "2rem",
 });
 
 export default function AboutMeText() {
@@ -24,6 +28,7 @@ export default function AboutMeText() {
       <Text>
         <Paragraph>{t("section.aboutme.first")}</Paragraph>
         <Paragraph>{t("section.aboutme.second")}</Paragraph>
+        <Paragraph>{t("section.aboutme.third")}</Paragraph>
       </Text>
     </TextContainer>
   );
